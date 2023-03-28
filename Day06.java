554aa4bb6ad2cab589751109998c2b2efd53b3d0
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-03-28 23:21:33 +0530

    Add files via upload

---
+++ Day06.java
@@ -0,0 +1,19 @@
+package ChaitraChallenge;
+
+import java.util.Scanner;
+
+public class Day06 {
+
+	public static void main(String arg[])	
+	{
+	    double a,c;
+        Scanner sc=new Scanner(System.in);	   	 
+	    System.out.println("Enter  Fahrenheit temperature");
+        a=sc.nextDouble(); 
+	    System.out.println("Celsius temperature is = "+celsius(a));		  	  	     
+	} 
+	static double celsius(double f)
+	{	
+	return  (f-32)*5/9;
+	}
+}
