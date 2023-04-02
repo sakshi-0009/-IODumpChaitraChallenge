8b9320264a3ddc66e59bb8d72f7068e257d9edfa
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-04-02 14:52:56 +0530

    Update Day11.java

--- Day11.java
+++ Day11.java
@@ -1,16 +1,18 @@
+// Write a Program to find length of String without using Inbuilt Function :
+
 package ChaitraChallenge;
 
 import java.io.*;
 public class Day11 {
-	public static void main(String args[]) throws Exception {
+   public static void main(String args[]) throws Exception {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter the String : ");
-	    String S = br.readLine();
-	    int i = 0;
-	    for(char C: S.toCharArray()) {
-	       i++;
-	    }
-	    System.out.println("Length of the given string ::"+i);
-	 }
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter the String : ");
+	String S = br.readLine();
+	int i = 0;
+	for(char C: S.toCharArray()) {
+	    i++;
+	}
+	System.out.println("Length of the given string ::"+i);
+   }
 }
